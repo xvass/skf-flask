a21059357072684e3b896eb4cab96778918c7bdb
blabla1337 <glenntencate@gmail.com>
2016-06-29 12:12:19 +0200

    Fix for passing travis

--- skf/core.py
+++ skf/core.py
@@ -21,8 +21,15 @@ def add_response_headers(headers={}):
     return decorator
 
 def security(f):
-    """This decorator passes multiple security headers and checks log file to block users"""
-    return add_response_headers({'X-Frame-Options': 'deny', 'X-XSS-Protection': '1', 'X-Content-Type-Options': 'nosniff', 'Cache-Control': 'no-store, no-cache','Strict-Transport-Security': 'max-age=16070400; includeSubDomains', 'Server': 'Security Knowledge Framework'})(f)
+    """This decorator passes multiple security headers"""
+    return add_response_headers({
+        'X-Frame-Options': 'deny', 
+        'X-XSS-Protection': '1', 
+        'X-Content-Type-Options': 'nosniff', 
+        'Cache-Control': 'no-store, no-cache',
+        'Pragma': 'no-cache',
+        'Strict-Transport-Security': 'max-age=16070400; includeSubDomains', 
+        'Server': 'Security Knowledge Framework'})(f)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5443)
